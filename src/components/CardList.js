840e4b4c40e63e0ea4639108db6fe483cd014e44
Sanjay <sanjaykviswas@gmail.com>
2020-05-29 18:34:33 +0530

    Complete the states without redux

---
+++ src/components/CardList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  const cardComponent = robots.map((user, index) => {
+    return (
+      <Card
+        key={index}
+        id={robots[index].id}
+        name={robots[index].name}
+        email={robots[index].email}
+      />
+    );
+  });
+  return <div>{cardComponent}</div>;
+};
+export default CardList;
