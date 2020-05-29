7510a62614c27623558a85d0514bb9d621f01d4f
Sanjay <sanjaykviswas@gmail.com>
2020-05-29 19:21:39 +0530

    Completed robofriend with build and without redux

---
+++ build/precache-manifest.90e8547eadbc9c1c89692a4415a93a60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ad80e3f7cb86ccc324cdb3751b658c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95a8d56d1caf91952705",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8c36b8bab386d80eb6d6",
+    "url": "/static/css/main.02b46118.chunk.css"
+  },
+  {
+    "revision": "95a8d56d1caf91952705",
+    "url": "/static/js/2.11bae53e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.11bae53e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c36b8bab386d80eb6d6",
+    "url": "/static/js/main.20fbfb5e.chunk.js"
+  },
+  {
+    "revision": "3a172e9daf29f6cc3071",
+    "url": "/static/js/runtime-main.08f98dcf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
